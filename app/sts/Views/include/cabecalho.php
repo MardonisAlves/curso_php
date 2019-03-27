ecfb8162b15a03468b0ae27d3e618cfb74600087
Mardonis Alves Bezerra <mardonis@dony-dev.dony-dev.com.br>
2019-03-27 00:00:02 -0300

    menu

--- app/sts/Views/include/cabecalho.php
+++ app/sts/Views/include/cabecalho.php
@@ -3,6 +3,10 @@
     <head>
         <meta charset="UTF-8">
         <title>Celke</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="icon" href="<?php echo URL; ?>assets/imagens/icone/favicon.ico">
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
+		<link rel="stylesheet" href="assets/css/ionicons.min.css">
+		<link rel="stylesheet" href="assets/css/personalizado.css">
     </head>
     <body>
--- app/sts/Views/include/rodape.php
+++ app/sts/Views/include/rodape.php
@@ -1,3 +1,7 @@
 
+    <script src="assets/js/jquery-3.2.1.slim.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+		<script src="assets/js/scrollreveal.min.js"></script>
     </body>
 </html>
